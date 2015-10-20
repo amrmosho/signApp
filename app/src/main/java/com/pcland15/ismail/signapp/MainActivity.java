c218680a6883b27bd0cb278d9bfcf6228fce6a8f
empcl_000 <empcland@gmail.com>
2015-10-20 22:26:02 +0200

    dfgdfdf

--- app/src/main/java/com/pcland15/ismail/signapp/MainActivity.java
+++ app/src/main/java/com/pcland15/ismail/signapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.pcland15.ismail.signapp;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.pcland15.ismail.signapp.libs.dbOperations;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        dbOperations db =new dbOperations();
+        db.type="get_row";
+
+
+        String d=db.commit();
+        Log.d("signData", d
+        );
+
+
+
     }
 }
